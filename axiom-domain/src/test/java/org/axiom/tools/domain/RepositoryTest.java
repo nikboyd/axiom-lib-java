77cf2513f54abc35796a2643d8811c0a5cdfce64
Nik Boyd <nikboyd@sonic.net>
2013-11-21 07:20:38 -0800

    brought project structure into maven conformance

---
+++ axiom-domain/src/test/java/org/axiom/tools/domain/RepositoryTest.java
@@ -0,0 +1,156 @@
+/**
+ * Copyright 2013 Nikolas Boyd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.axiom.tools.domain;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Confirms proper operation of sample domain classes and their
+ * respective repositories.
+ */
+public class RepositoryTest {
+	
+	@Test
+//	@Ignore
+	public void basicPhones() {
+		PhoneNumber n = PhoneNumber.from("888-888-8888");
+		assertTrue(n.formatNumber().equals("888-888-8888"));
+	}
+	
+	@Test
+//	@Ignore
+	public void phoneStability() {
+		PhoneNumber n = PhoneNumber.from("888-888-8888").save();
+		PhoneNumber p = PhoneNumber.from("888-888-8888").save();
+		assertTrue(n.getKey() == p.getKey());
+		assertTrue(n.getHashKey() == p.getHashKey());
+		
+		PhoneNumber x = PhoneNumber.from("888-888-8888").find();
+		assertTrue(x != null);
+		assertTrue(x.getKey() == p.getKey());
+	}
+	
+	@Test
+//	@Ignore
+	public void addressStability() {
+		StreetAddress a = StreetAddress.with("1234 Main St", "Anytown", "CA", "94005").save();		
+		StreetAddress b = StreetAddress.with("1234 Main St", "Anytown", "CA", "94005").find();
+
+		assertTrue(b != null);
+		assertTrue(b.getKey() == a.getKey());
+		assertTrue(b.getHashKey() == a.getHashKey());
+		a.describe();
+		b.describe();
+	}
+
+	@Test
+//	@Ignore
+	public void componentLifecycle() {
+		StreetAddress a = StreetAddress.with("1234 Main St", "Anytown", "CA", "94005").save();
+		
+		assertTrue(a != null);
+		assertTrue(a.getKey() > 0);
+		
+		StreetAddress b = a.reload();
+		assertTrue(b != null);
+		assertTrue(b.getKey() > 0);
+		a.describe();
+		b.describe();
+		
+		b = b.withCity("Uptown").save();
+		assertTrue(b != null);
+		assertFalse(b.getKey() == a.getKey());
+		assertFalse(b.getHashKey() == a.getHashKey());
+		assertFalse(b.getCity().equals(a.getCity()));
+		b.describe();
+
+		assertTrue(b.remove());
+		assertTrue(b.reload() == null);
+	}
+	
+	@Test
+//	@Ignore
+	public void compositeLifecycle() {
+		System.out.println("phone count = " + PhoneNumber.count());
+		System.out.println("email count = " + EmailAddress.count());
+		System.out.println("address count = " + StreetAddress.count());
+		System.out.println("contact count = " + Contact.count());
+
+		PhoneNumber p = PhoneNumber.from("415-717-2158").save();
+		EmailAddress e = EmailAddress.from("nikboyd@sonic.net").save();
+
+		StreetAddress a = StreetAddress.with("1234 Main St", "Anytown", "CA", "94005").save();
+		StreetAddress b = StreetAddress.with("1234 Main St", "Anytown", "CA", "94005").save();
+
+		Contact c = 
+			new Contact()
+				.addAddress(Contact.Type.HOME, a)
+				.addAddress(Contact.Type.WORK, b)
+				.addEmail(Contact.Type.HOME, e)
+				.addPhone(Contact.Type.HOME, p)
+				.save();
+
+		assertTrue(c != null);
+		assertTrue(c.getKey() > 0);
+		assertTrue(c.countPhones() > 0);
+		assertTrue(c.countAddresses() > 0);
+		
+		System.out.println("phone count = " + PhoneNumber.count());
+		System.out.println("email count = " + EmailAddress.count());
+		System.out.println("address count = " + StreetAddress.count());
+		System.out.println("contact count = " + Contact.count());
+		
+		Contact d = c.reload();
+		assertTrue(d != null);
+		assertTrue(d.getKey() > 0);
+		assertTrue(c.countPhones() > 0);
+		assertTrue(d.countAddresses() > 0);
+		
+		a = b.find();
+		assertTrue(a != null);
+		assertTrue(a.getKey() > 0);
+		assertTrue(a.getHashKey() == b.getHashKey());
+		
+		PhoneNumber n = d.getPhone(Contact.Type.HOME);
+		assertTrue(n != null);
+		assertTrue(n.getKey() > 0);
+		assertTrue(n.formatNumber().equals(p.formatNumber()));
+		
+		PhoneNumber pn = n.find();
+		assertTrue(pn != null);
+		assertTrue(pn.getKey() > 0);
+		assertTrue(pn.formatNumber().equals(p.formatNumber()));
+		
+		b = d.getAddress(Contact.Type.WORK);
+		assertTrue(b != null);
+		assertTrue(b.getKey() > 0);
+		assertTrue(b.getCity().equals(a.getCity()));
+		
+		d.removeAddress(Contact.Type.WORK);
+
+		assertTrue(d.remove());
+		assertTrue(d.reload() == null);
+		assertTrue(b.reload() == null);
+		
+		System.out.println("phone count = " + PhoneNumber.count());
+		System.out.println("email count = " + EmailAddress.count());
+		System.out.println("address count = " + StreetAddress.count());
+		System.out.println("contact count = " + Contact.count());
+	}
+
+} // RepositoryTest
